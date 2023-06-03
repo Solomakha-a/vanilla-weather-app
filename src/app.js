dd62a1d1e51f1a00302272200b6d135b63e552ec
Solomakha-a <135162324+Solomakha-a@users.noreply.github.com>
2023-06-03 14:14:02 +0100

    updated apiurl info

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 let apiKey = "4o2b1et2ad8780b3de6b1ffa54355c3a";
-let apiUrl ="https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units={units}";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Moscow&key=4o2b1et2ad8780b3de6b1ffa54355c3a&units=metric`;
+console.log(apiUrl);
+
+
+function displayTemperature(response) {
+    console.log(response.data.condition.description);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#query");
+    cityElement.innerHTML = response.data.city;
+    let conditionElement = document.querySelector("#condition");
+    conditionElement = response.data.condition.description;
+    
+}
+axios.get(apiUrl).then(displayTemperature);

36f181c5b5d80ecb714f4c3ee0f3d8ed69700e7b
Solomakha-a <135162324+Solomakha-a@users.noreply.github.com>
2023-06-03 17:06:29 +0100

    Updated api descriptions

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 let apiKey = "4o2b1et2ad8780b3de6b1ffa54355c3a";
-let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Moscow&key=4o2b1et2ad8780b3de6b1ffa54355c3a&units=metric`;
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Paris&key=4o2b1et2ad8780b3de6b1ffa54355c3a&units=metric`;
 console.log(apiUrl);
 
 
 function displayTemperature(response) {
-    console.log(response.data.condition.description);
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#query");
     cityElement.innerHTML = response.data.city;
     let conditionElement = document.querySelector("#condition");
-    conditionElement = response.data.condition.description;
+    conditionElement.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `Humidity ${response.data.temperature.humidity}%`;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = `Wind ${Math.round(response.data.wind.speed)} km/h`;
     
 }
 axios.get(apiUrl).then(displayTemperature);

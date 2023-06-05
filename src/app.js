3ec7f6f3b51fa80114df8921a3f2d799b5594a6a
Solomakha-a <135162324+Solomakha-a@users.noreply.github.com>
2023-06-05 08:33:04 +0100

    Implemented unit conversion

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     let cityElement = document.querySelector("#query");
     cityElement.innerHTML = response.data.city;
     let conditionElement = document.querySelector("#condition");
@@ -32,6 +32,8 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute ("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     iconElement.setAttribute("alt", response.data.condition.description);
+
+    celsiusTemperature = response.data.temperature.current;
 }
 
 function search(city) {
@@ -39,14 +41,40 @@ let apiKey = "4o2b1et2ad8780b3de6b1ffa54355c3a";
 let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 }
+
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
 
-search("London");
+function displayFarTemperature(event) {
+    event.preventDefault();
+    let farTemperature = (celsiusTemperature * 9) / 5 + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(farTemperature);
+    celLinkElement.classList.remove("active");
+    farLinkElement.classList.add("active");
+}
 
+function displayCelTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    celLinkElement.classList.add("active");
+    farLinkElement.classList.remove("active");
+}
+
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-input");
 form.addEventListener("submit", handleSubmit);
+
+let farLinkElement = document.querySelector("#far-link");
+farLinkElement.addEventListener("click", displayFarTemperature);
+
+let celLinkElement = document.querySelector("#celsius-link");
+celLinkElement.addEventListener("click", displayCelTemperature);
+
+search("London");
